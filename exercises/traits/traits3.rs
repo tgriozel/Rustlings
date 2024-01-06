8b410f60f508020acb9eb73775452e84fff91eb0
Thomas Griozel <thomas.griozel@gmail.com>
2024-01-06 10:44:49 +0100

    Traits

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,10 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 struct SomeSoftware {
@@ -33,9 +33,7 @@ mod tests {
     fn is_licensing_info_the_same() {
         let licensing_info = String::from("Some information");
         let some_software = SomeSoftware { version_number: 1 };
-        let other_software = OtherSoftware {
-            version_number: "v2.0.0".to_string(),
-        };
+        let other_software = OtherSoftware { version_number: "v2.0.0".to_string() };
         assert_eq!(some_software.licensing_info(), licensing_info);
         assert_eq!(other_software.licensing_info(), licensing_info);
     }
--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint traits4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub trait Licensed {
     fn licensing_info(&self) -> String {
         "some information".to_string()
@@ -23,7 +21,7 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
+fn compare_license_types<A: Licensed, B: Licensed>(software: A, software_two: B) -> bool {
     software.licensing_info() == software_two.licensing_info()
 }
 
